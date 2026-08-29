e5671b48cc961732f97adf29eaa46dc2cd225777
Violet <191287039+violet678@users.noreply.github.com>
2026-08-29 16:10:21 -0300

    codigo com mais variaveis que aprendi

---
+++ c++/Type_Declaration.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+main () {
+    //declarando variaveis
+    int count = 10;
+    double total = 150.75;
+    char grade = 'A';
+    bool isActive = false;
+    string userName = "Bob123";
+    //imprimindo valores
+    cout << "Count: " << count;
+    cout << "Total: " << total;
+    cout << "Grade: " << grade;
+    cout << "Ativo: " << isActive;
+    cout << "nome de usuario: " << userName;
+
+    
+    return 0;
+}
